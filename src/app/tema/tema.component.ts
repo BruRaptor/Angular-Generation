eda2bc976a3633dd359a4edac2ea13a1f624338e
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-02-23 23:13:51 -0300

    @BruRaptor [Adicionando componente inicio e tema]

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
-import { AuthService } from '../service/auth.service';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
 })
-export class InicioComponent implements OnInit {
+export class TemaComponent implements OnInit {
 
   constructor(
-    private auth: AuthService,
     private router: Router
   ) { }
 

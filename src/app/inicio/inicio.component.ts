e74e4fbde0f025a6ee5e9136310477f0e03c1729
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-03-04 22:46:48 -0300

    @BruRaptor [Adicionando o Crud Postagem]

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
 import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -10,9 +15,21 @@ import { AuthService } from '../service/auth.service';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
+
   constructor(
-    private auth: AuthService,
-    private router: Router
+    private authService: AuthService,
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -22,6 +39,49 @@ export class InicioComponent implements OnInit {
       alert('Sua seção expirou para sua segurança! Faça o login novamente!')
       this.router.navigate(['/entrar'])
     }
+    this.authService.refreshToken();
+    this.getAllTemas();
+    this.getAllPostagens();
   }
 
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+
+  findByIdUsuario(){
+    this.authService.getByIdUsuario(this.idUsuario).subscribe((resp: Usuario)=>{
+      this.usuario = resp
+    })
+  }
+
+  
+  publicar(){
+
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) =>{
+      this.postagem = resp
+      alert('Postagem cadastrada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
+  }
 }
